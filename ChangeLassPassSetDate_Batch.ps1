afa0b0a8221c4bc7a92198f8aeb6de542ff73b63
hugo2k24 <167785885+hugo2k24@users.noreply.github.com>
2024-05-19 10:43:16 +0300

    Add files via upload
    Смена даты последней смены пароля пользователя на текущую дату.

---
+++ ChangeLassPassSetDate_Batch.ps1
@@ -0,0 +1,8 @@
+﻿$UserCSV = Import-Csv -Path "C:\PS\PasswordBatchChange\passchange_2007.csv" -Delimiter ";" -Encoding UTF8
+
+    foreach ($i in $UserCSV)
+        {
+           
+            Set-ADUser  $i.SamAccountName  -Replace @{pwdLastSet='0'}
+            Set-ADUser  $i.SamAccountName  -Replace @{pwdLastSet='-1'}
+        }
