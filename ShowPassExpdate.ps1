f677dd50d1b246c4274659ea00df2844685e0924
hugo2k24 <167785885+hugo2k24@users.noreply.github.com>
2024-05-19 10:45:59 +0300

    Add files via upload
    Выгрузка даты окончания срока действия пароля пользователей

---
+++ ShowPassExpdate.ps1
@@ -0,0 +1,3 @@
+﻿$Userlogin = "panteleeva.loren"
+$user= get-aduser -Identity $Userlogin -Properties msDS-UserPasswordExpiryTimeComputed
+([DateTime]::FromFileTime($user.“msDS-UserPasswordExpiryTimeComputed”))
